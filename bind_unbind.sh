3f930971877ca54777566ef11d714797678e3577
SchnuBby2205 <dominik.muche@outlook.com>
2025-04-23 14:11:53 +0200

    scripts / new howto
    Neue Howto geschrieben und vereinfacht.
    2 Scripts zum testen der VFIO Einstellungen und zum binden der GPU.

---
+++ bind_unbind.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+gpu="0000:03:00.0"
+aud="0000:03:00.1"
+gpu_vd="$(cat /sys/bus/pci/devices/$gpu/vendor) $(cat /sys/bus/pci/devices/$gpu/device)"
+aud_vd="$(cat /sys/bus/pci/devices/$aud/vendor) $(cat /sys/bus/pci/devices/$aud/device)"
+
+function bind_vfio {
+  echo "$gpu" > "/sys/bus/pci/devices/$gpu/driver/unbind"
+  echo "$aud" > "/sys/bus/pci/devices/$aud/driver/unbind"
+  echo "$gpu_vd" > /sys/bus/pci/drivers/vfio-pci/new_id
+  echo "$aud_vd" > /sys/bus/pci/drivers/vfio-pci/new_id
+}
+ 
+function unbind_vfio {
+  echo "$gpu_vd" > "/sys/bus/pci/drivers/vfio-pci/remove_id"
+  echo "$aud_vd" > "/sys/bus/pci/drivers/vfio-pci/remove_id"
+  echo 1 > "/sys/bus/pci/devices/$gpu/remove"
+  echo 1 > "/sys/bus/pci/devices/$aud/remove"
+  echo 1 > "/sys/bus/pci/rescan"
+  echo "$gpu" > "/sys/bus/pci/devices/$gpu/driver/bind"
+  echo "$aud" > "/sys/bus/pci/devices/$aud/driver/bind"
+}
+
+case $1 in 
+  amd) unbind_vfio;;
+  vfio) bind_vfio;;
+  *) exit 0;;
+esac
